16d6ee50d07435e317b0fde6e111d0fd1e0f6394
pkleineb <mail@paul-kleineberg.de>
2024-08-25 15:00:12 +0200

    hyprland is making me angry and can't source the files although it should be able to follow the symlinks

--- nixos/modules/home-manager/hyprland/default.nix
+++ nixos/modules/home-manager/hyprland/default.nix
@@ -1,15 +1,18 @@
-{ ... }:
+{ config, ... }:
 
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland = {
-      enable = true;
-    };
-  };
-
-  home.file.".config/hypr/" = {
+  home.file."./.config/hypr/" = {
     recursive = true;
-    source = ./hypr/;
+    source = ./hypr;
+    text = ''
+      source = ${config.home.homeDirectory}/animations.conf
+      source = ${config.home.homeDirectory}/keybindings.conf
+      source = ${config.home.homeDirectory}/windowrules.conf
+      source = ${config.home.homeDirectory}/themes/common.conf # shared theme settings
+      source = ${config.home.homeDirectory}/themes/theme.conf # theme specific settings
+      source = ${config.home.homeDirectory}/themes/colors.conf # wallbash color override
+      source = ${config.home.homeDirectory}/monitors.conf # initially empty, to be configured by user and remains static
+      source = ${config.home.homeDirectory}/userprefs.conf # initially empty, to be configured by user and remains static
+    '';
   };
 }
