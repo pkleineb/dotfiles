7b2c566862f6c7502b77db900a097dc08a30784c
pkleineb <mail@paul-kleineberg.de>
2024-08-25 15:17:19 +0200

    this actually works now but rebooting my system and rebuilding nixos on change in hyprland settings is super stupid

--- nixos/modules/home-manager/hyprland/default.nix
+++ nixos/modules/home-manager/hyprland/default.nix
@@ -4,15 +4,5 @@
   home.file."./.config/hypr/" = {
     recursive = true;
     source = ./hypr;
-    text = ''
-      source = ${config.home.homeDirectory}/animations.conf
-      source = ${config.home.homeDirectory}/keybindings.conf
-      source = ${config.home.homeDirectory}/windowrules.conf
-      source = ${config.home.homeDirectory}/themes/common.conf # shared theme settings
-      source = ${config.home.homeDirectory}/themes/theme.conf # theme specific settings
-      source = ${config.home.homeDirectory}/themes/colors.conf # wallbash color override
-      source = ${config.home.homeDirectory}/monitors.conf # initially empty, to be configured by user and remains static
-      source = ${config.home.homeDirectory}/userprefs.conf # initially empty, to be configured by user and remains static
-    '';
   };
 }
