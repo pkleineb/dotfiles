dc7b0107c32d72bbe070238c84ac7b36f448c815
pkleineb <mail@paul-kleineberg.de>
2024-09-04 13:23:00 +0200

    added dolphin for file explorer

--- nixos/modules/home-manager/file_browser/dolphin/default.nix
+++ nixos/modules/home-manager/file_browser/dolphin/default.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   home.packages = with pkgs; [
     dolphin
   ];
+
+  home.file."./.config/dolphinrc".source = config.lib.meta.mk_out_of_store_symlink ./dolphinrc;
 }
