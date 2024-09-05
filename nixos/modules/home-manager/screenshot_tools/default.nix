a0fb2bfc0f49b2d902ff758ba15d811b27ea27ec
pkleineb <mail@paul-kleineberg.de>
2024-09-05 21:22:07 +0200

    adding screenshot ability and gimp

---
+++ nixos/modules/home-manager/screenshot_tools/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, inputs, ... }: 
+let
+  import_dirs = inputs.self.outputs.scripts.import_all_modules;
+in
+{
+  imports = import_dirs { dir=./.; };
+
+  home.packages = with pkgs; [
+    satty
+  ];
+}
