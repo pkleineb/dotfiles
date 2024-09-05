f12ba25f39f93d6df517ac2825e8fb0e35692eb8
pkleineb <mail@paul-kleineberg.de>
2024-09-05 19:47:38 +0200

    using oh-my-posh in replacement of p10k

--- nixos/modules/home-manager/shells/zsh/default.nix
+++ nixos/modules/home-manager/shells/zsh/default.nix
@@ -1,21 +1,62 @@
 { config, lib, pkgs, ... }:
-
+let
+  oh_my_posh_config_file = "oh-my-posh/config.toml";
+in
 {
   options = {
   };
 
   config = {
+    home.packages = with pkgs; [
+      oh-my-posh
+    ];
+
+    home.file."./.config/${oh_my_posh_config_file}" = {
+      recursive = true;
+      source = config.lib.meta.mk_out_of_store_symlink "./${oh_my_posh_config_file}";
+    };
+
     programs.zsh = {
       enable = true;
+
+      plugins = [
+        {
+          name = "zsh-256color";
+          src = pkgs.fetchFromGitHub {
+            owner = "chrissicool";
+            repo = "zsh-256color";
+            rev = "master";
+            sha256 = "1fk84n29vi7ndxhmc8asxkwd2bhgacsimffqj2cw9c366aj5pyiz";
+          };
+        }
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.7.0";
+            sha256 = "1g3pij5qn2j7v7jjac2a63lxd97mcsgw6xq6k5p7835q9fjiid98";
+          };
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.8.0";
+            sha256 = "1yl8zdip1z9inp280sfa5byjbf2vqh2iazsycar987khjsi5d5w8";
+          };
+        }
+      ];
 
       oh-my-zsh = {
         enable = true;
         plugins = [
           "git"
           "sudo"
-          "zsh-256color"
-          "zsh-autosuggestions"
-          "zsh-syntax-highlighting"
+          #"zsh-256color"
+          #"zsh-autosuggestions"
+          #"zsh-syntax-highlighting"
         ];
       };
 
@@ -47,10 +88,8 @@
       initExtra = ''
         eval "$(atuin init zsh)"
         eval "$(zoxide init zsh)"
+        eval "$(oh-my-posh init zsh --config ./.config/${oh_my_posh_config_file})"
       '';
     };
-
-    #home.file.".zshrc".source = config.lib.meta.mk_out_of_store_symlink ./.zshrc;
-    #home.file.".zshenv".source = config.lib.meta.mk_out_of_store_symlink ./.zshenv;
   };
 }
