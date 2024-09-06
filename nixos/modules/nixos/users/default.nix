0efba419bad330cd7eedfa1f5739e6af67aadebc
pkleineb <mail@paul-kleineberg.de>
2024-09-06 11:51:42 +0200

    adding docker to system

--- nixos/modules/nixos/users/default.nix
+++ nixos/modules/nixos/users/default.nix
@@ -20,17 +20,12 @@ in
       enable = true;
     };
 
-    system.activationScripts.zlinkZsh = ''
-      mkdir -p /usr/bin
-      ln -sfn ${pkgs.zsh}/bin/zsh /usr/bin/zsh
-    '';
-
     users.users.${user_config.user_name} = {
       isNormalUser = true;
       shell = pkgs.zsh;
       initialPassword = "12345";
       description = user_config.user_name;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "networkmanager" "docker" ];
     };
 
   };
