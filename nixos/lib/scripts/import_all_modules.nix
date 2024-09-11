02f8a1d65c959aa1ac1d4758e92230615752f5e3
pkleineb <mail@paul-kleineberg.de>
2024-09-11 12:27:34 +0200

    restructured for better code readability

--- nixos/lib/scripts/import_all_modules.nix
+++ nixos/lib/scripts/import_all_modules.nix
@@ -7,13 +7,19 @@ let
     )
     (builtins.readDir dir);
 
-  files = dir: lib.collect lib.isString (lib.mapAttrsRecursive (path: type: lib.concatStringsSep "/" path) (get_dir dir));
+  files = dir:
+    let
+      dir_contents = get_dir dir;
+      flattened_files = lib.collect lib.isString (lib.mapAttrsRecursive (path: type: lib.concatStringsSep "/" path) dir_contents);
+    in
+    flattened_files;
 
-  valid_files = dir : lib.map
-    (file : dir + "/${file}")
-    (lib.filter
-      (file: lib.hasSuffix ".nix" file)
-      (files dir)
-    );
+  valid_files = dir : 
+    let
+      all_files = files dir;
+      filtered_files = lib.filter (file: lib.hasSuffix "default.nix" file) all_files;
+      result = lib.map (file: "${dir}/${file}") filtered_files;
+    in
+      result;
 in
   valid_files dir
