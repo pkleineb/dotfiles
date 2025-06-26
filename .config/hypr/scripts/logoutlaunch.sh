b63057e3fe43fd2193ab96a30926efdb39112681
pkleineb <mail@paul-kleineberg.de>
2025-06-26 11:56:09 +0200

    shuffeld some layout stuff needs so much more work

--- .config/hypr/scripts/logoutlaunch.sh
+++ .config/hypr/scripts/logoutlaunch.sh
@@ -10,13 +10,8 @@ fi
 # set file variables
 ScrDir=`dirname "$(realpath "$0")"`
 source $ScrDir/globalcontrol.sh
-wLayout="${XDG_CONFIG_HOME:-$HOME/.config}/wlogout/layout_$1"
-wlTmplt="${XDG_CONFIG_HOME:-$HOME/.config}/wlogout/style_$1.css"
-
-if [ ! -f $wLayout ] || [ ! -f $wlTmplt ] ; then
-    echo "ERROR: Config $1 not found..."
-    exit 1;
-fi
+wLayout="${XDG_CONFIG_HOME:-$HOME/.config}/wlogout/layout"
+wlTmplt="${XDG_CONFIG_HOME:-$HOME/.config}/wlogout/style.css"
 
 # detect monitor res
 x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
@@ -25,16 +20,9 @@ hypr_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale'
 
 
 # scale config layout and style
-case $1 in
-    1)  wlColms=6
-        export mgn=$(( y_mon * 28 / hypr_scale ))
-        export hvr=$(( y_mon * 23 / hypr_scale )) ;;
-    2)  wlColms=2
-        export x_mgn=$(( x_mon * 35 / hypr_scale ))
-        export y_mgn=$(( y_mon * 25 / hypr_scale ))
-        export x_hvr=$(( x_mon * 32 / hypr_scale ))
-        export y_hvr=$(( y_mon * 20 / hypr_scale )) ;;
-esac
+wlColms=6
+export mgn=$(( y_mon * 28 / hypr_scale ))
+export hvr=$(( y_mon * 23 / hypr_scale ))
 
 # scale font size
 export fntSize=$(( y_mon * 2 / 100 ))
