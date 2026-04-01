d29020c4f9a3d378188114fca70ff083b8bc3f1f
pkleineb <mail@paul-kleineberg.de>
2026-04-01 10:03:04 +0200

    added rustaceannvim to vim

--- .config/nvim/lua/plugins/rustaceannvim/init.lua
+++ .config/nvim/lua/plugins/rustaceannvim/init.lua
@@ -2,4 +2,19 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6",
     lazy = false,
+
+    config = function ()
+        vim.g.rustaceanvim = {
+            server = {
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            allFeatures = true,
+                        }
+                    }
+                }
+            }
+        }
+        
+    end
 }
