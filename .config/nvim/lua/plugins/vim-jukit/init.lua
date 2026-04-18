27f8ee909c92caf538dc2e197b3ebc471e41e23d
pkleineb <mail@paul-kleineberg.de>
2026-04-18 23:38:58 +0200

    added jukit keybinds

--- .config/nvim/lua/plugins/vim-jukit/init.lua
+++ .config/nvim/lua/plugins/vim-jukit/init.lua
@@ -1,4 +1,13 @@
 return {
     "luk400/vim-jukit",
-    ft = "python",
+    ft = {"py", "ipynb"},
+
+    keys = {
+        -- Remove original binding
+        { "<leader>ts", false },
+        -- Add new binding
+        { "<leader>jt", function() vim.fn["jukit#splits#term"]() end, desc = "Jukit Terminal Split" },
+        { "<leader>os", function() vim.fn["jukit#splits#output"]() end, desc = "Jukit Open Output" },
+        { "<leader>np", function() vim.fn["jukit#convert#notebook_convert"]("jupyter-notebook") end, desc = "Jukit convert notebook file" },
+    },
 }
